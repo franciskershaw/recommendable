bf13cb59d8cdd09241d4bacebf1dd0172853b693
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-08-10 15:31:47 +0100

    Tweak import rules

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,4 @@
-/** @type {import('prettier'.Config)} */
+/** @type {import('prettier').Config} */
 module.exports = {
   endOfLine: "lf",
   semi: true,
@@ -6,10 +6,28 @@ module.exports = {
   tabWidth: 2,
   trailingComma: "es5",
   importOrder: [
+    // React imports first
     "^(react/(.*)$)|^(react$)",
+
     "",
+    // Packages from node_modules (third-party libraries)
     "<THIRD_PARTY_MODULES>",
-    "^[.]",
+
+    "",
+    // Aliased imports (matches any alias starting with @)
+    "^@/(.*)$",
+
+    "",
+    // Absolute imports from internal project folders (if any)
+    "^src/(.*)$",
+
+    "",
+    // Style imports
+    "^.+\\.s?css$",
+
+    "",
+    // Relative imports (e.g., ./ and ../)
+    "^[./]",
   ],
   plugins: ["@ianvs/prettier-plugin-sort-imports"],
 };
