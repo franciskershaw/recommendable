6cbe10e34b4226cf23cd87967493626aededeced
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-08-10 16:39:51 +0100

    configure shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import path from "path";
+
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
@@ -15,6 +17,7 @@ export default defineConfig({
       "@utils": "/src/utils",
       "@styles": "/src/styles",
       // Add more aliases as needed
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
