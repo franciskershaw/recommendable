2cb8e3178954f3763bb62e5c3bc84639d39bee78
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-08-10 13:56:07 +0100

    Install tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import TanstackProvider from "./tanstackQuery/TanstackProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <TanstackProvider>
+      <App />
+    </TanstackProvider>
   </StrictMode>
 );
