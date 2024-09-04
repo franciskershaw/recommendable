c8f177aaa46aa68a8236f13673f7b6bab148d471
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-09-04 21:48:04 +0100

    Remove comments

--- src/components/ui/Sidebar/Sidebar.tsx
+++ src/components/ui/Sidebar/Sidebar.tsx
@@ -24,16 +24,13 @@ const Sidebar = () => {
         isExpanded ? "w-64" : "w-20"
       } h-full bg-slate-700 flex flex-col py-8 transition-all duration-500 relative`}
     >
-      {/* Top Section: Settings + Auth */}
       <div className="flex flex-col items-center space-y-8">
-        {/* Admin/Settings */}
         <SidebarIcon
           icon={<FaCog />}
           label="Settings"
           isExpanded={isExpanded}
         />
 
-        {/* Log In/Log Out */}
         <SidebarIcon
           icon={<FaSignInAlt />}
           label="Log In"
@@ -41,7 +38,6 @@ const Sidebar = () => {
         />
       </div>
 
-      {/* Middle Section: Main Categories */}
       <div className="flex flex-col items-center space-y-12 flex-grow justify-center">
         <SidebarIcon icon={<FaFilm />} label="Films" isExpanded={isExpanded} />
         <SidebarIcon icon={<FaTv />} label="TV" isExpanded={isExpanded} />
@@ -53,7 +49,6 @@ const Sidebar = () => {
         />
       </div>
 
-      {/* Bottom Section: Toggle button */}
       <button
         className={`absolute bottom-4 right-[-15px] transform bg-gray-600 p-2 rounded-full focus:outline-none transition-transform duration-500`}
         onClick={toggleSidebar}
@@ -68,7 +63,6 @@ const Sidebar = () => {
   );
 };
 
-// Component for individual icons
 const SidebarIcon = ({
   icon,
   label,
@@ -80,12 +74,10 @@ const SidebarIcon = ({
 }) => {
   return (
     <div className="flex items-center w-full">
-      {/* Icon */}
       <div className="flex-shrink-0 w-20 flex justify-center">
         <span className="text-white text-xl">{icon}</span>
       </div>
 
-      {/* Label */}
       <span
         className={`${
           isExpanded ? "opacity-100" : "opacity-0"
