4915eed2ee8b07a6ff2b9dc429a3fae42d510c0f
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-09-06 21:10:04 +0100

    Add placeholder pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import SharedLayout from "./components/layout/SharedLayout";
 import Auth from "./pages/Auth/Auth";
+import Events from "./pages/Categories/Events/Events";
+import Films from "./pages/Categories/Films/Films";
+import Music from "./pages/Categories/Music/Music";
+import Travel from "./pages/Categories/Travel/Travel";
+import TV from "./pages/Categories/TV/TV";
 
 function App() {
   return (
@@ -11,6 +16,11 @@ function App() {
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Auth />} />
+          <Route path="/films" element={<Films />} />
+          <Route path="/TV" element={<TV />} />
+          <Route path="/music" element={<Music />} />
+          <Route path="/travel" element={<Travel />} />
+          <Route path="/events" element={<Events />} />
         </Route>
       </Routes>
     </Router>
