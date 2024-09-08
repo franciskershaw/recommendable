e86be6bbb9987109195f31039f6d2443d93a2c20
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-09-08 11:12:00 +0100

    Add fontWeight to heading

--- src/components/ui/Heading/Heading.tsx
+++ src/components/ui/Heading/Heading.tsx
@@ -3,9 +3,23 @@ import { ReactNode } from "react";
 interface HeadingProps {
   children: ReactNode;
   type?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  fontWeight?:
+    | "font-thin"
+    | "font-extralight"
+    | "font-light"
+    | "font-normal"
+    | "font-medium"
+    | "font-semibold"
+    | "font-bold"
+    | "font-extrabold"
+    | "font-black";
 }
 
-const Heading = ({ children, type = "h1" }: HeadingProps) => {
+const Heading = ({
+  children,
+  type = "h1",
+  fontWeight = "font-normal",
+}: HeadingProps) => {
   const headingStyles = {
     h1: "text-4xl",
     h2: "text-3xl",
@@ -16,7 +30,7 @@ const Heading = ({ children, type = "h1" }: HeadingProps) => {
   };
 
   const Tag = type;
-  const styles = headingStyles[type];
+  const styles = `${headingStyles[type]} ${fontWeight}`;
 
   return <Tag className={styles}>{children}</Tag>;
 };
