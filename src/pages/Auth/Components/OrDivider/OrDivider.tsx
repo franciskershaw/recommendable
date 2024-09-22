3b11098f3c7d8358cf0f1c6ca812c145314c8ede
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-09-22 10:19:35 +0100

    Or divider

---
+++ src/pages/Auth/Components/OrDivider/OrDivider.tsx
@@ -0,0 +1,11 @@
+const OrDivider = () => {
+  return (
+    <div className="flex items-center my-6 w-full">
+      <hr className="flex-grow border-gray-300" />
+      <span className="px-3 text-gray-500">or</span>
+      <hr className="flex-grow border-gray-300" />
+    </div>
+  );
+};
+
+export default OrDivider;
