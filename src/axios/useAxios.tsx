9028306ab9631203616840d4beb7b2986c18695b
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-09-25 22:07:19 +0100

    Get started with interceptors

--- src/axios/useAxios.tsx
+++ src/axios/useAxios.tsx
@@ -1,15 +1,53 @@
-import axios from "axios";
+import { useQueryClient } from "@tanstack/react-query";
+import axios, { AxiosResponse } from "axios";
+import { useNavigate } from "react-router-dom";
+
+import queryKeys from "@/tanstackQuery/queryKeys";
+import { User } from "@/types/globalTypes";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: true, // Moved out of headers
+  withCredentials: true,
 });
 
 const useAxios = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
   const api = axiosInstance;
+
+  api.interceptors.response.use(
+    (response: AxiosResponse) => response,
+    async (error) => {
+      if (
+        error.response.status === 401 &&
+        (error.response.data.errorCode === "SESSION_EXPIRED" ||
+          error.response.data.errorCode === "INVALID_TOKEN")
+      ) {
+        try {
+          const originalRequest = error.config;
+          const response = await api.get("/auth/refresh-token");
+          originalRequest.headers.Authorization = `Bearer ${response.data.accessToken}`;
+          queryClient.setQueryData<User>([queryKeys.user], (oldData) => {
+            if (!oldData) {
+              return oldData;
+            }
+            return {
+              ...oldData,
+              accessToken: response.data.accessToken,
+            };
+          });
+          return api(originalRequest);
+        } catch {
+          queryClient.setQueryData([queryKeys.user], null);
+          navigate("/");
+        }
+      }
+    }
+  );
 
   return api;
 };
