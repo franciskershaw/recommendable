9ba0476c5287f18dec13efc775dbc2ba9a202388
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-09-29 09:41:52 +0100

    Install tooltip and add to providers

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
+import { TooltipProvider } from "./components/ui/Tooltip/Tooltip.tsx";
 import TanstackProvider from "./tanstackQuery/TanstackProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <TanstackProvider>
-      <App />
+      <TooltipProvider>
+        <App />
+      </TooltipProvider>
     </TanstackProvider>
   </StrictMode>
 );
