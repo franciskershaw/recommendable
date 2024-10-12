6ab410aca406e233284937044bc1bc94db5976ad
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2024-10-12 22:12:03 +0100

    Fix hover effect on primary buttons

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -10,7 +10,8 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default:
+          "bg-primary text-primary-foreground hover:bg-primary/90 hover:text-secondary-foreground hover:border hover:border-secondary-foreground",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
